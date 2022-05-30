61d7a2f28ede241bfe3ab1bdc7e3783907238ac2
obamo1 <joobamo@gmail.com>
2022-05-30 15:42:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Index from './tutorial/10-prop-types/setup/index';
+ 
+ReactDOM.render(
+    <Index />,
+  
+  document.getElementById('root')
+);
